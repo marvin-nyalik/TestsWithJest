e8b62036207abdc914dbf62f3352091355236228
MARVIN <o.silverygold@gmail.com>
2023-06-13 15:50:22 +0300

    Update fibonacci function

--- fibonacci.js
+++ fibonacci.js
@@ -8,7 +8,7 @@ function fibonacci(n) {
     if(n === 1) { return 1;}
     if(n === 2) { return 1;}
 
-    else return fibonacci (n) + fibonacci(n-1);
+    else return fibonacci (n-1) + fibonacci(n-2);
 }
 
 module.exports = fibonacci;
